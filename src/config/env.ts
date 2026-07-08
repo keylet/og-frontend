c249ad5a7ce2f8c31648cf7383f70177d096254d
keylet <alexpazos88@gmail.com>
2026-07-07 20:08:41 -0500

    Backend-cloud

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,4 +12,4 @@
 
 export const CLERK_PUBLISHABLE_KEY = "pk_test_dHJ1ZS1tZWVya2F0LTUxLmNsZXJrLmFjY291bnRzLmRldiQ";
 
-export const BACKEND_URL = "http://192.168.100.4:8000";
+export const BACKEND_URL = "https://rapazos-orientago-backend.hf.space";
